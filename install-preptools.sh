1f29ac69c56f23def3188ece4b7b8e61c41e3521
Christian Lanz <6087393+rekt0x@users.noreply.github.com>
2019-11-13 08:11:59 +0100

    Create install-preptools.sh

---
+++ install-preptools.sh
@@ -0,0 +1,10 @@
+sudo apt-get install -y libssl-dev build-essential automake pkg-config libtool libffi-dev libgmp-dev libyaml-cpp-dev
+sudo apt-get install -y python3.7-dev libsecp256k1-dev python3-pip
+sudo apt-get install python3-venv
+git clone https://github.com/icon-project/preptools.git
+cd preptools
+python3 -m venv venv
+source venv/bin/activate
+./build.sh
+pip install dist/preptools-1.0.2-py3-none-any.whl
+pip install preptools
